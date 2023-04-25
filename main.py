1a24445642973315bd0e551effa35676e25d533a
Edward Elton <usermail@proton.me>
2023-04-25 17:48:06 -0400

    feat: init

---
+++ main.py
@@ -0,0 +1,34 @@
+import os, asyncio
+
+from discord.ext import commands
+from dotenv import load_dotenv
+
+bot = commands.Bot(command_prefix='.', self_bot=True)
+
+@bot.event
+async def on_ready():
+    print('x')
+
+@bot.command()
+async def c(ctx, limit: int = 1000):
+    removed = 0
+    tried = 0
+
+    async for msg in ctx.message.channel.history(limit = limit):
+        if msg.author.id == bot.user.id:
+            if (tried % 15 == 0 and tried != 0):
+                print("Sleeping for 3 seconds...")
+                await asyncio.sleep(3)
+                
+            try:
+                await msg.delete()
+                removed += 1
+            except:
+                pass
+            tried += 1
+        asyncio.sleep(0.3)
+
+    print(f"Removed {removed} messages. Now waiting.")
+
+load_dotenv()
+bot.run(os.getenv('TOKEN'))
