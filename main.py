7d92737ec1d414864a01cf3e16c40964a1e28097
e <103802645+302007@users.noreply.github.com>
2023-08-05 04:05:41 -0700

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os, asyncio
 from discord.ext import commands
 from dotenv import load_dotenv
 
-bot = commands.Bot(command_prefix='.', self_bot=True)
+bot = commands.Bot(command_prefix='.', self_bot=True, help_command=None)
 
 @bot.event
 async def on_ready():
@@ -13,22 +13,15 @@ async def on_ready():
 async def c(ctx, limit: int = 1000):
     removed = 0
     tried = 0
-
-    async for msg in ctx.message.channel.history(limit = limit):
-        if msg.author.id == bot.user.id:
-            if (tried % 10 == 0 and tried != 0):
-                print("Sleeping for 3 seconds...")
-                await asyncio.sleep(3)
-                
+    async for msg in ctx.channel.history(limit = limit):
             try:
                 await msg.delete()
                 removed += 1
             except:
                 pass
             tried += 1
-        await asyncio.sleep(0.3)
 
-    print(f"Removed {removed} messages. Now waiting.")
+    print(f"Successfully Removed {removed} messages.")
 
 load_dotenv()
 bot.run(os.getenv('TOKEN'))
